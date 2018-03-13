259c19013c916a979fcb2ce656262abe6d2f51e8
pmessina <pmm0501@gmail.com>
2018-03-13 17:36:21 -0400

    wrap code in run method

--- patrick.py
+++ patrick.py
@@ -9,17 +9,18 @@ import csv as cs
 x = []
 y = []
 
-file_name = ".\\DrivingData.csv"
+def run()
+    file_name = ".\\DrivingData.csv"
 
-with open(file_name) as csvfile:
-    next(csvfile)
-    plots =  cs.reader(csvfile, delimiter=',')
-    for row in plots:
-        x.append(float(row[20]))
-        y.append(float(row[21]))
-plt.plot(x, y, label='Loaded from file')
-plt.xlabel('Atmospheric Pressure')
-plt.ylabel('Sound Level')
-plt.title('Atmospheric Pressure vs Sound Level')
-plt.legend()
-plt.show()      
+    with open(file_name) as csvfile:
+        next(csvfile)
+        plots =  cs.reader(csvfile, delimiter=',')
+        for row in plots:
+            x.append(float(row[20]))
+            y.append(float(row[21]))
+    plt.plot(x, y, label='Loaded from file')
+    plt.xlabel('Atmospheric Pressure')
+    plt.ylabel('Sound Level')
+    plt.title('Atmospheric Pressure vs Sound Level')
+    plt.legend()
+    plt.show()      
